adf1279ebec9a4ca9304c8c85230aad49d206c3e
Hèctor Martínez <46947878+hecmaralcaraz@users.noreply.github.com>
2019-05-29 15:42:05 +0200

    Update start_laboratroy.sh

--- start_laboratroy.sh
+++ start_laboratroy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 cd server
-time vagrant up >> ../.log_start.log
+vagrant up >> ../.log_start.log
 
 cd ../client
-time vagrant up >> ../.log_start.log
-time vagrant reload >> ../.log_start.log
+vagrant up >> ../.log_start.log
 
 cd ../tester
-time vagrant up >> ../.log_start.log
-time vagrant >> ../.log_start.log
+vagrant up >> ../.log_start.log
 clear
 echo "ok"
